be8837b884073452259d7fa35a12ad0026403ee6
Mikołaj Tomalik <tomalikem@op.pl>
2020-10-21 17:00:02 +0200

    changes

--- src/main/scala/EShop/lab2/Cart.scala
+++ src/main/scala/EShop/lab2/Cart.scala
@@ -3,7 +3,7 @@ package EShop.lab2
 case class Cart(items: Seq[Any]) {
   def contains(item: Any): Boolean = items.contains(item)
   def addItem(item: Any): Cart     = Cart(items :+ item)
-  def removeItem(item: Any): Cart  = Cart(items.filterNot(i: i == item))
+  def removeItem(item: Any): Cart  = Cart(items.filterNot(_ == item))
   def size: Int                    = items.size
 }
 
